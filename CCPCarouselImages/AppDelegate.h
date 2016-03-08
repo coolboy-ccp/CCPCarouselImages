162f28a08732baa1f12624eb4a8185d1e9ebad08
brian-chu <chuchengpeng@liqunfeidemac-mini.local>
2016-03-08 14:35:22 +0800

    Initial Commit

---
+++ CCPCarouselImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCPCarouselImages
+//
+//  Created by liqunfei on 16/3/8.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCPCarouselImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCPCarouselImages
+//
+//  Created by liqunfei on 16/3/8.
+//  Copyright © 2016年 chuchengpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
